9948c893ee4fff9c34a1c856c3164a2a27e65e85
raymundd <rayd.9100@gmail.com>
2021-08-01 10:44:46 +0300

    Update StartSSDR_RDX6500.bat

--- StartSSDR_RDX6500.bat
+++ StartSSDR_RDX6500.bat
@@ -1,3 +1,23 @@
+@REM startSSDR_RDX6500.bat
+@REM
+@REM Copyright 2021 Ray Delaforce
+@REM
+@REM This program is free software: you can redistribute it and/or modify
+@REM it under the terms of the GNU General Public License as published by
+@REM the Free Software Foundation, either version 3 of the License, or
+@REM (at your option) any later version.
+@REM
+@REM This program is distributed in the hope that it will be useful,
+@REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+@REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+@REM GNU General Public License for more details.
+@REM
+@REM You should have received a copy of the GNU General Public License
+@REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@REM 
+@REM Starting an instance of SmartSDR using a pre-determined settings file
+@REM This is just so I can control the initial STATION name.
+@REM Author: R.G.Delaforce G6UJB.
 @REM Starting an instance of SmartSDR using a pre-determined settings file
 @REM This is just so I can control the initial STATION name.
 @REM Author: R.G.Delaforce G6UJB.
@@ -10,7 +30,7 @@
 @REM Set another unique STATION name and then exit SmartSDR.
 @REM Save the file %APPDATA%\FlexRadio Systems\SSDR.settings to another file with a unique name.
 @REM Now alter the script to match the location and names of the saved copies of settings file.
-
+@REM
 @REM Script now Copies the current settings to safer place - this is not entirely necessary since losing
 @REM the settings file is not the end of the World, SmartSDR will just create a new one with defaults.
 copy /Y "%APPDATA%\FlexRadio Systems\SSDR.settings" "%APPDATA%\FlexRadio Systems\SSDR_Orig.settings"
